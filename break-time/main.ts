f901d722b9bc8418eb3b7164adb08ff90b8ba990
Scott Roberts <s.e.roberts0@gmail.com>
2025-06-25 23:01:35 -0700

    Basic logging of Discord user activity

--- break-time/main.ts
+++ break-time/main.ts
@@ -1,12 +1,39 @@
 import { load } from "https://deno.land/std@0.212.0/dotenv/mod.ts";
-import { createBot } from "@discordeno/bot";
+import { createBot, Intents } from "@discordeno/bot";
 
 const env = await load();
 
 const bot = createBot({
   token: env.discord_bot_token,
+  intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent | Intents.GuildPresences,
   events: {
-    ready: ({ shardId }) => console.log(`Shard ${shardId} ready`),
+    ready(payload) {
+      console.log(payload)
+    },
+    messageCreate(message) {
+      console.log(message)
+    },
+    typingStart(payload) {
+      console.log(payload)
+    },
+    channelUpdate(channel) {
+      console.log(channel);
+    },
+    presenceUpdate(presence) {
+      console.log(presence)
+    }
+  },
+  desiredProperties: {
+    message: {
+      id: true,
+      author: true,
+      content: true,
+    },
+    user: {
+      id: true,
+      toggles: true, // Toggles includes the "bot" flag
+      username: true,
+    },
   },
 });
 
